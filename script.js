6fd204eae1efa281e7f8e32dc4b05a13260bc7cd
Mossaka <jiz417@ucsd.edu>
2017-05-06 18:09:27 -0700

    integrate Bing image search API to add image to the background

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 $(document).ready(function() {
-	
-
-	$("#button").on('click', function() {
+    $("#button").on('click', function() {
 		setQuote();
+
 	});
 
 	/*var setQuote = function() {
@@ -24,21 +23,61 @@ $(document).ready(function() {
 
 	//setQuote();
 
+
 	var setQuote = function() {
 		$.getJSON('quotes.json').done( function(data) {
 
-			var randInt = Math.floor(Math.random() * data['quotes'].length)
-			var quoteObj = data['quotes'][randInt]
+			var randInt = Math.floor(Math.random() * data['quotes'].length);
+			var quoteObj = data['quotes'][randInt];
 
-			var anime = quoteObj.anime
-			var author = quoteObj.author
-			var quote = quoteObj.quote
+			var anime = quoteObj.anime;
+			var author = quoteObj.author;
+			var quote = quoteObj.quote;
 
-			var sQuote = encodeURIComponent(quote)
-			$('#quote').text(quote)
-			$('#author').text(author)
-			$('#anime').text(anime)
-			$('.btnTweet').attr('href', "https://twitter.com/intent/tweet?text=" + sQuote)
+			var sQuote = encodeURIComponent(quote);
+			$('#quote').text(quote);
+			$('#author').text("-- " + author);
+			$('#anime').text(anime);
+			$('.btnTweet').attr('href', "https://twitter.com/intent/tweet?text=" + sQuote);
+
+            keyword = anime;
+
+            /*$.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
+                {
+                    tags: keyword,
+                    tagmode: "any",
+                    format: "json"
+                },
+                function(data) {
+                    var rnd = Math.floor(Math.random() * data.items.length);
+
+                    var image_src = data.items[0]['media']['m'].replace("_m", "_b");
+
+                    $('body').css('background-image', "url('" + image_src + "')");
+
+                });*/
+            var parameter = {
+                "q": author
+            };
+
+            var url = "https://api.cognitive.microsoft.com/bing/v5.0/images/search?";
+
+            $.ajax({
+                url: url + $.param(parameter),
+                type: "GET",
+                beforeSend: function(xhrObj) {
+                    xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","c960a1eea23a46059bbbaff5a8358656")
+                },
+                data: ""
+            })
+                .done(function(data) {
+                	var image_src = data.value[0].contentUrl;
+                    $('body').css('background-image', "url('" + image_src + "')");
+                })
+                .fail(function() {
+                    alert("error");
+                });
+
 		});
 	};
 });
